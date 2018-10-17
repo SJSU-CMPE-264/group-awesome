25557cd021b3415d76831e21b3298232ac57c1a4
nick <frangoose@gmail.com>
2018-10-17 12:30:22 -0700

    added write enable

--- param_dreg.v
+++ param_dreg.v
@@ -1,14 +1,15 @@
-module param_dreg(d, q, clk, rst);
+module param_dreg(d, q, we, clk, rst);
 
 parameter width = 32; //Can be set to 1, 10, 23 etc.
 
 input [width-1:0] d;
 output [width-1:0] q;
-input clk, rst;
+input we, clk, rst;
 
 always@(posedge CLK) begin
 	if(rst) q = 0;
-	else q = d;
+	else if(we) q = d;
+	else q = q;
 end
 
 endmodule		
