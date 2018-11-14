fc67dd7557f5d428b5a93fe8ecddbcdebff036f2
Alex Norell <alex.norell@gmail.com>
2018-11-13 18:14:10 -0800

    Updated the test bench

--- FPMul_tb.v
+++ FPMul_tb.v
@@ -1,10 +1,35 @@
-module ;
-  wire [7:0] value;
-  counter c1 (value, clk, reset);
+module FPMul;
+    reg         clk, rst, Start;
+    reg [31:0]  A, B;
+    wire        Done, OF, UF, NanF, InfF, DNF, ZF;
+    wire [31:0] P;
 
-initial
- begin
-    $dumpfile("test.vcd");
-    $dumpvars(0,test);
- end
+    FPMul DUT(clk, rst, Start, A, B, Done, P, OF, UF, NanF, InfF, DNF, ZF);
+
+
+    task tick;
+        begin
+            #5 clk = 1;
+            #5 clk = 0;
+        end
+    endtask
+
+    task reset; 
+        begin 
+            #5 rst = 1; 
+            #5 rst = 0; 
+        end 
+    endtask
+
+    initial begin
+        reset;
+        A=5;
+        B=5;
+        Start = 1;
+        tick;
+        tick;
+        tick;
+        $dumpfile("test.vcd");
+        $dumpvars(0,test);  
+    end
 endmodule // 
