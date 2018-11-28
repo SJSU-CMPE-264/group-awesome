0869879ce8a95c5fac558c57542905d18f1c2241
Kevin Server <kevinserver@yahoo.com>
2018-11-27 16:53:31 -0800

    Added code for ISRs
    The code is hardcoded into the imem. It is still untested.
    Will be working on testing it next.

--- single_cycle_proc/Decoder.v
+++ single_cycle_proc/Decoder.v
@@ -9,18 +9,18 @@ module decoder
     always @ (we, a)
     begin
         casex ({we, a})
-            33'h1_0000_00xx: begin wem = 1; we1 = 0; we2 = 0; we3 = 0; end
-            33'h1_0000_080x: begin wem = 0; we1 = 1; we2 = 0; we3 = 0; end
-            33'h1_0000_090x: begin wem = 0; we1 = 0; we2 = 1; we3 = 0; end
-            33'h1_0000_0A0x: begin wem = 0; we1 = 0; we2 = 0; we3 = 1; end
+            33'h1_0000_00xx: begin wem = 1; we1 = 0; we2 = 0; we3 = 0; end //memory
+            33'h1_0000_080x: begin wem = 0; we1 = 1; we2 = 0; we3 = 0; end //factorial
+            33'h1_0000_090x: begin wem = 0; we1 = 0; we2 = 1; we3 = 0; end //gpio
+            33'h1_0000_0A0x: begin wem = 0; we1 = 0; we2 = 0; we3 = 1; end //FPMul    
             default: begin wem = 0; we1 = 0; we2 = 0; we3 = 0; end
         endcase
         
         casex (a)
-            32'h0000_000x: begin rdsel = 1; end
-            32'h0000_080x: begin rdsel = 2; end
-            32'h0000_090x: begin rdsel = 3; end
-            33'h0000_0A0x: begin rdsel = 0; end
+            32'h0000_000x: begin rdsel = 1; end //memory
+            32'h0000_080x: begin rdsel = 2; end //factorial
+            32'h0000_090x: begin rdsel = 3; end //gpio
+            33'h0000_0A0x: begin rdsel = 0; end //FPMul    
             default: begin rdsel = 0; end
         endcase
     end
